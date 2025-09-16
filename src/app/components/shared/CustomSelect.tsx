04e088c2fbb0e86561fc200a60aa7e64518b0c73
herbertDev12 <herbertn740@gmail.com>
2025-09-16 17:07:31 -0500

    feat: add the basic project structure.

---
+++ src/app/components/shared/CustomSelect.tsx
@@ -0,0 +1,22 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select"
+
+export function CustomSelect({  }) {
+  return (
+    <Select>
+        <SelectTrigger className="w-[100px] bg-gray-800">
+            <SelectValue placeholder="Units" />
+        </SelectTrigger>
+        <SelectContent>
+            <SelectItem value="light">Celcius(C)</SelectItem>
+            <SelectItem value="dark">Fahrenheit(F)</SelectItem>
+            <SelectItem value="system">Wind Speed (km/h)</SelectItem>
+        </SelectContent>
+    </Select>
+  )
+}
