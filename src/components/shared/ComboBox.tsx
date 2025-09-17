fdf6b94ba18b7617f8a0148ff076c8a866e69944
herbertDev12 <herbertn740@gmail.com>
2025-09-17 15:26:40 -0500

    fix(combobox): change the color backgound of the popoover content

--- src/components/shared/ComboBox.tsx
+++ src/components/shared/ComboBox.tsx
@@ -19,26 +19,26 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 
-const frameworks = [
+const countries = [
   {
-    value: "next.js",
-    label: "Next.js",
+    value: "Cuba",
+    label: "Cuba",
   },
   {
-    value: "sveltekit",
-    label: "SvelteKit",
+    value: "Argentina",
+    label: "Argentina",
   },
   {
-    value: "nuxt.js",
-    label: "Nuxt.js",
+    value: "Mexico",
+    label: "Mexico",
   },
   {
-    value: "remix",
-    label: "Remix",
+    value: "Colombia",
+    label: "Colombia",
   },
   {
-    value: "astro",
-    label: "Astro",
+    value: "Chile",
+    label: "Chile",
   },
 ]
 
@@ -56,21 +56,21 @@ export function Combobox() {
           className="w-[200px] justify-between bg-gray-800"
         >
           {value
-            ? frameworks.find((framework) => framework.value === value)?.label
-            : "Select framework..."}
+            ? countries.find((country) => country.value === value)?.label
+            : "Search for a place..."}
           <ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
-        <Command>
+      <PopoverContent className="w-[200px] p-0 ">
+        <Command className="bg-gray-800 ">
           <CommandInput placeholder="Search for a place..." />
-          <CommandList>
-            <CommandEmpty>No framework found.</CommandEmpty>
+          <CommandList className="text-white">
+            <CommandEmpty>No country found.</CommandEmpty>
             <CommandGroup>
-              {frameworks.map((framework) => (
+              {countries.map((country) => (
                 <CommandItem
-                  key={framework.value}
-                  value={framework.value}
+                  key={country.value}
+                  value={country.value}
                   onSelect={(currentValue) => {
                     setValue(currentValue === value ? "" : currentValue)
                     setOpen(false)
@@ -79,10 +79,10 @@ export function Combobox() {
                   <CheckIcon
                     className={cn(
                       "mr-2 h-4 w-4",
-                      value === framework.value ? "opacity-100" : "opacity-0"
+                      value === country.value ? "opacity-100" : "opacity-0"
                     )}
                   />
-                  {framework.label}
+                  {country.label}
                 </CommandItem>
               ))}
             </CommandGroup>
